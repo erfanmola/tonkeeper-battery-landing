c5ac7aace591ebceb586818c5bc8672f8c774508
Erfan Mola <hi@erfanxd.ir>
2024-06-30 11:29:02 +0330

    Add new locales

--- src/locale.ts
+++ src/locale.ts
@@ -1,46 +1,73 @@
 import type * as en from "./i18n/en.ts";
 import * as i18n from "@solid-primitives/i18n";
 
-// import { dict as ar_dict } from "./i18n/ar.ts";
+import { dict as ar_dict } from "./i18n/ar.ts";
 import { dict as en_dict } from "./i18n/en.ts";
-
-// import { dict as fa_dict } from "./i18n/fa.ts";
-// import { dict as ru_dict } from "./i18n/ru.ts";
+import { dict as es_dict } from "./i18n/es.ts";
+import { dict as fa_dict } from "./i18n/fa.ts";
+import { dict as hi_dict } from "./i18n/hi.ts";
+import { dict as id_dict } from "./i18n/id.ts";
+import { dict as ru_dict } from "./i18n/ru.ts";
+import { dict as zhHans_dict } from "./i18n/zh-Hans.ts";
 
 const locales = [
     "en",
-    // "ru",
-    // "fa",
-    // "ar"
+    "ru",
+    "fa",
+    "ar",
+    "es",
+    "id",
+    "hi",
+    "zhHans",
 ] as const;
 
 const localeFlags: { [key in Locale]: string } = {
     'en': '🇺🇸',
-    // 'fa': '🇮🇷',
-    // 'ru': '🇷🇺',
-    // 'ar': '🇦🇪',
+    'fa': '🇮🇷',
+    'ru': '🇷🇺',
+    'ar': '🇦🇪',
+    'es': '🇪🇸',
+    'id': '🇮🇩',
+    'hi': '🇮🇳',
+    'zhHans': '🇨🇳',
 };
 
 const localeDirections: { [key in Locale]: string } = {
     'en': 'ltr',
-    // 'fa': 'rtl',
-    // 'ru': 'ltr',
-    // 'ar': 'rtl',
+    'fa': 'rtl',
+    'ru': 'ltr',
+    'ar': 'rtl',
+    'es': 'ltr',
+    'id': 'ltr',
+    'hi': 'ltr',
+    'zhHans': 'ltr',
 };
 
-async function fetchDictionary(locale: Locale): Promise<Dictionary> {
+function fetchDictionary(locale: Locale): Dictionary {
     let dict: RawDictionary;
 
     switch (locale) {
-        // case 'fa':
-        //     dict = fa_dict;
-        //     break;
-        // case 'ar':
-        //     dict = ar_dict;
-        //     break;
-        // case 'ru':
-        //     dict = ru_dict;
-        //     break;
+        case 'fa':
+            dict = fa_dict;
+            break;
+        case 'ar':
+            dict = ar_dict;
+            break;
+        case 'ru':
+            dict = ru_dict;
+            break;
+        case 'es':
+            dict = es_dict;
+            break;
+        case 'id':
+            dict = id_dict;
+            break;
+        case 'hi':
+            dict = hi_dict;
+            break;
+        case 'zhHans':
+            dict = zhHans_dict;
+            break;
         default:
             dict = en_dict;
             break;
